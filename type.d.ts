49d80566c2225ddf48dd55225c0eb2547ef7bb72
Shafin <blackipie@outlook.com>
2023-08-04 19:34:10 +0530

    init

--- type.d.ts
+++ type.d.ts
@@ -12,3 +12,14 @@ type Meta = {
 type NoteType = {
     meta: Meta,
 }
+
+type ActiveUser = {
+   createdAt: string;
+    id: string;
+    name: string | null;
+    email: string | null;
+    phone: string | null;
+    password: string | null;
+    emailVerified: Date | null;
+    image: string | null;
+} | any
